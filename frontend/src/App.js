3b16d8dccd8a7fc0ff7ad48cf2c8e9d7fc9b6cc2
pdccheung <pdccheung@gmail.com>
2021-05-08 13:09:49 -0400

    products details page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
-import HomeScreen from "./pages/HomeScreen";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import ProductPage from "./pages/ProductPage"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/product/:id' component={ProductPage} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
