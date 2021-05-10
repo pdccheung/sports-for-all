6dbffb3c5bb5c4b7d80d0cae327bf062261ebdef
pdccheung <pdccheung@gmail.com>
2021-05-10 14:36:11 -0400

    redux

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,34 +1,34 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { listProducts } from "../actions/prodcutActions";
 
-
-
-
 const HomePage = () => {
-  const dispatch = useDispatch()
-  const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-  useEffect(() => { 
-    dispatch(listProducts())
-  }, [dispatch])
-
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest products</h1>
-      { loading? <h2>loading...</h2>: error ? <h3>{error}</h3> :   
-           <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>}
-
+      {loading ? (
+        <h2>loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
