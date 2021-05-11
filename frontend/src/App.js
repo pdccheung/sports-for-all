deee4bcecde72193c02ebef10caceb9b0643c391
pdccheung <pdccheung@gmail.com>
2021-05-11 14:54:49 -0400

    cart page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import ProductPage from "./pages/ProductPage"
+import CartPage from "./pages/CartPage";
 
 const App = () => {
   return (
@@ -13,7 +14,9 @@ const App = () => {
         <Container>
           <Route exact path='/' component={HomePage} />
           <Route path='/product/:id' component={ProductPage} />
+          <Route path='/cart/:id?' component={CartPage} />
         </Container>
+   
       </main>
       <Footer />
     </Router>
