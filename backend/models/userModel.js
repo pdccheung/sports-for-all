87188a4623d589efb6bacea31a43b1eed1d8a37d
pdccheung <pdccheung@gmail.com>
2021-05-12 14:46:58 -0400

    user routes added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+
 
 const userSchema = mongoose.Schema({
     name:{ type: String, required: true},
@@ -7,7 +9,12 @@ const userSchema = mongoose.Schema({
     isAdmin:{ type: Boolean, required: true, default: false},
 }, {
     timestamps: true
-})
+}
+)
+
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model("User", userSchema)
 export default User
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import userRoutes from "./routes/userRoutes.js"
+
 
 const app = express()
+app.use(express.json())
+
 dotenv.config()
 
 connectDB()
@@ -15,6 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
