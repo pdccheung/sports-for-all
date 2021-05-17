52ed458a37bf5731fe5044efd1346fb346dc080f
pdccheung <pdccheung@gmail.com>
2021-05-17 12:07:16 -0400

    order creation feature

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-
 import { cartReducer } from "./reducers/cartReducers"
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from "./reducers/userReducers" 
+import { orderCreateReducer } from "./reducers/orderReducers"
 
 
 const reducer = combineReducers({
@@ -15,6 +15,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')):[]
