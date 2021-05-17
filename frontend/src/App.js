758464755b103c5f9bf1f261c1d61f33c02ceb00
pdccheung <pdccheung@gmail.com>
2021-05-17 13:09:48 -0400

    order details page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import ProductPage from "./pages/ProductPage"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage"
+import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import ShippingPage from "./pages/ShippingPage";
 import PaymentPage from "./pages/PaymentPage";
 import PlaceOrderPage from "./pages/PlaceOrderPage";
+import OrderPage from "./pages/OrderPage";
 
 const App = () => {
   return (
@@ -18,16 +19,16 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path='/product/:id' component={ProductPage} />
-          <Route path='/shipping' component={ShippingPage} />
-          <Route path='/payment' component={PaymentPage} />
-          <Route path='/placeorder' component={PlaceOrderPage} />
-          <Route path='/cart/:id?' component={CartPage} />
-          <Route path='/login' component={LoginPage} />
-          <Route path='/register' component={RegisterPage} />
-          <Route path='/profile' component={ProfilePage} />
-          <Route exact path='/' component={HomePage} />
-
+          <Route path="/order/:id" component={OrderPage} />
+          <Route path="/shipping" component={ShippingPage} />
+          <Route path="/payment" component={PaymentPage} />
+          <Route path="/placeorder" component={PlaceOrderPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/product/:id" component={ProductPage} />
+          <Route path="/cart/:id?" component={CartPage} />
+          <Route exact path="/" component={HomePage} />
         </Container>
       </main>
       <Footer />
